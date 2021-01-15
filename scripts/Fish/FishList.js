c17b3c7818dd8956d9f12761edf23fea6b8c211c
Dylan Morris <dylanrobertmorris@gmail.com>
2021-01-15 13:38:21 -0600

    refactored code into categories

--- scripts/Fish/FishList.js
+++ scripts/Fish/FishList.js
@@ -1,20 +1,44 @@
-import { useFish } from './FishDataProvider.js'
+import { useFish, mostHolyFish, nonHolyFish, soldierFish } from './FishDataProvider.js'
 import { Fish } from "./Fish.js"
+
+
+
+const buildFishListHTML = (arrayOfFish, heading) => {
+    let fishHTMLRepresentations = `<h3>${heading}<h3>`
+    for (const fishObj of arrayOfFish) {
+        fishHTMLRepresentations += Fish(fishObj)
+        console.log(fishHTMLRepresentations)
+    }
+    return fishHTMLRepresentations
+}
+
+
 
 export const FishList = () => {
     const contentElement = document.querySelector(".containerLeft")
-    const fishes = useFish()
+    // const fishes = useFish()
 
-    let fishHTMLRepresentations = ""
-    for (const fish of fishes) {
-        fishHTMLRepresentations += Fish(fish)
+    // let fishHTMLRepresentations = ""
+    // for (const fish of fishes) {
+    //     fishHTMLRepresentations += Fish(fish)
         
-    }
+    // }
+
+    const holyFishArray = mostHolyFish()
+    const holyFishHTMLRepresentations = buildFishListHTML(holyFishArray, "Holy Fish")
+    
+    const soldierFishArray = soldierFish()
+    const soldierFishHTMLRepresentations = buildFishListHTML(soldierFishArray, "Soldier Fish")
+    
+    const regularFishArray = nonHolyFish()
+    const regularFishHTMLRepresentations = buildFishListHTML(regularFishArray, "Regular Fish")
 
 
     contentElement.innerHTML += `
         <article class="containerLeft__fishList contentArticle">
-            ${fishHTMLRepresentations}
+            ${holyFishHTMLRepresentations}
+            ${soldierFishHTMLRepresentations}
+            ${regularFishHTMLRepresentations}
         </article> 
     `
 }
